4bac8b75146456590cfe9631b5e7ace2e080c3c2
lhm <3186834196@qq.com>
2018-04-02 19:11:55 +0800

    Initial commit

---
+++ app/src/urlonline/java/com/channelas/Constant.java
@@ -0,0 +1,12 @@
+package com.channelas;
+
+/**
+ * author：lhm on 2018/4/1 16:57
+ * <p>
+ * email：3186834196@qq.com
+ */
+public class Constant {
+ //   public final static String URL = "主机环境:---10.18.3.1";
+    public final static String URL = "线上环境:---10.18.3.1";
+//    public final static String URL = "测试环境:---10.18.3.1";
+}
